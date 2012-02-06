ecd2c34ce8f3cecc7a993dc100c0b56c02a909a8
Marco Metz <marco.metz@gmail.com>
2012-02-06 15:33:14 +0100

    migations

--- test/dummy/db/migrate/20120206095607_add_event_to_articles.goldencobra_events.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from goldencobra_events (originally 20120201150701)
-class AddEventToArticles < ActiveRecord::Migration
-  def change
-    add_column :goldencobra_articles, :event_id, :integer
-
-  end
-end
--- test/dummy/db/migrate/20120206111754_add_panel_to_goldencobra_events_events.goldencobra_events.rb
+++
@@ -1,7 +0,0 @@
-# This migration comes from goldencobra_events (originally 20120206111715)
-class AddPanelToGoldencobraEventsEvents < ActiveRecord::Migration
-  def change
-    add_column :goldencobra_events_events, :panel_id, :integer
-
-  end
-end
