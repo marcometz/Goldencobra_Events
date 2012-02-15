5ee8b0d2f2e43bcd668af6efc8c1748f7b1acdb3
Holger Frohloff <holger.frohloff@gmail.com>
2012-02-15 17:58:25 +0100

    [#24920103] check for parents almost done. grandfathers missing

--- app/models/goldencobra_events/event_registration.rb
+++ app/models/goldencobra_events/event_registration.rb
@@ -21,67 +21,117 @@ module GoldencobraEvents
     before_save :is_registerable?
 
     def is_registerable?(list_of_registrations=nil)
-      #
       # receives array of event_pricegroup_ids and checks them for 
-      #
+      list_of_ids = []
       if list_of_registrations == nil
-        list_of_registrations = []
         if self.user && self.user.present? && self.user.event_registration_ids
-          list_of_registrations << self.user.event_registration_ids
+          list_of_ids << self.user.event_registration_ids
         end
-        list_of_registrations << self.id
+        list_of_ids << self.id
+      else
+        list_of_ids = list_of_registrations
       end
+
+      list_of_ids = list_of_ids.flatten.uniq
 
       error_msgs = {}
       mybool = true
-      list_of_registrations.each do |reg|
-      #
-      # is registration date valid?
-      #
-#        if !reg.registration_date_valid
- #         error_msgs << {:error => "Registration date is not valid"}
-  #      end
-      #
-      # is registration possible or is event mutually exclusive?
-      #
+      list_of_ids.each do |reg|
+        ereg = GoldencobraEvents::EventRegistration.find_by_id(reg)
+        if ereg
+          # is registration date valid?
+          unless ereg.registration_date_valid
+            error_msgs[:date_error] = "Registration date is not valid"
+          end
 
-      #
-      # max number of (event) participants reached?
-      #
-      
-      #
-      # max number of (pricegroup) participants reached?
-      #
+          # max number of (event) participants reached?
+          if ereg.max_number_of_events_participants_reached?
+            error_msgs[:num_of_ev_part_reached] = "Maximum number of participants reached for #{ereg.event_pricegroup.event.title}"
+          end
 
-      #
-      # parent needs_registration? && registration_done?
-      #
+          # max number of (pricegroup) participants reached?
+          if ereg.max_number_of_pricegroup_participants_reached?
+            error_msgs[:num_of_pricegroup_part_reached] = "Maximum number of participants reached for #{ereg.event_pricegroup.pricegroup.title}"
+          end
+          
+          # parent needs_registration? && registration_done?
+          if ereg.parent_registration_completed? != true
+            rtn_val = ereg.parent_registration_completed?
+            error_msgs[:parent_error] = rtn_val
+          end
+        end
 
-        mybool = GoldencobraEvents::EventRegistration.find(reg).registration_date_valid
+        # is registration possible or is event mutually exclusive?
       end
-      return mybool
-   #   return error_msgs.length > 0 ? false : true
+      return error_msgs.length > 0 ? error_msgs : true
     end
 
-
-    def self.registration_date_valid
+    def registration_date_valid
       # Check if current date is valid for registration
-      return start_date < Time.now && end_date > Time.now
+      return self.event_pricegroup.event.start_date < Time.now && 
+             self.event_pricegroup.event.end_date > Time.now if self.event_pricegroup && self.event_pricegroup.event
     end
 
     def registration_possible?
       # If siblings exist and if they are exclusive
       # Then check their registered? status
-      
     end
 
-    def max_number_of_participants_reached?
-      
+    def max_number_of_events_participants_reached?
+      event = self.event_pricegroup.event
+      if GoldencobraEvents::EventRegistration.joins(:event_pricegroup)
+                                             .where("goldencobra_events_event_pricegroups.event_id = #{event.id}")
+                                             .select("goldencobra_events_event_registrations.id")
+                                             .count <= event.max_number_of_participators
+        return false
+      else
+        return true
+      end
     end
 
-    def parent_registration_done?
-      
+    def max_number_of_pricegroup_participants_reached?
+      if GoldencobraEvents::EventRegistration.where("event_pricegroup_id = #{self.event_pricegroup_id}")
+                                             .select("goldencobra_events_event_registrations.id")
+                                             .count <= self.event_pricegroup.max_number_of_participators
+        return false
+      else
+        return true
+      end
     end
-    
+
+    def parent_registration_completed?(*args)
+      # does parent need a registration? Is registration already in system?
+      a = []
+      a.concat(args)
+      event = self.event_pricegroup.event
+      if event.parent.present?
+        parent = event.parent
+        if parent.type_of_event == "Registration needed"
+          if self.user && self.user.present?
+            # If user and parent with "Registration needed" present, look for EventRegistration for User/Parent
+            if GoldencobraEvents::EventRegistration.joins(:event_pricegroup)
+                                                   .where("goldencobra_events_event_registrations.user_id = #{self.user.id} AND goldencobra_events_event_pricegroups.event_id = #{parent.id}")
+                                                   .select("goldencobra_events_event_registrations.id")
+                                                   .count < 1
+              a << parent.id
+            end
+            if parent.parent.present?
+              args = a.join(",")
+              parent.parent_registration_completed?(args)
+            else
+              return a
+            end
+          end
+        else # "parent doesn't need a registration"
+          if parent.parent.present?
+            parent.parent_registration_completed?(a.join(","))
+          else
+            return a.length > 0 ? a : true
+          end
+        end
+      else # no parent present
+        return true
+      end
+    end
   end
 end
--- test/dummy/spec/models/event_registration_spec.rb
+++ test/dummy/spec/models/event_registration_spec.rb
@@ -2,16 +2,79 @@ require 'spec_helper'
 
 describe GoldencobraEvents::EventRegistration do
   before(:each) do
-    @event = GoldencobraEvents::Event.create!
-    @event_pricegroup = GoldencobraEvents::EventPricegroup.create!
-    @event_pricegroup.event = @event
+    @event = GoldencobraEvents::Event.create!(:start_date=>(Date.today - 1.week), 
+                                              :end_date => (Date.today + 1.day),
+                                              :title => "Mein Event",
+                                              :max_number_of_participators => 10)
+    pricegroup = GoldencobraEvents::Pricegroup.create(:title => "Studenten")
+
+    @event_pricegroup = GoldencobraEvents::EventPricegroup.create(:max_number_of_participators => 10)
+    @event_pricegroup.event_id = @event.id
+    @event_pricegroup.pricegroup_id = pricegroup.id
+    @event_pricegroup.save
+
     @new_registration = GoldencobraEvents::EventRegistration.new
     @new_registration.event_pricegroup = @event_pricegroup
+    @user = User.create!(:email=>"test@test.de",
+                         :password=>"secure12",
+                         :password_confirmation=>"secure12")
+    @new_registration.user = @user
+    @new_registration.save
+    @user2 = User.create!(:email=>"second_tester@test.de",
+                         :password=>"secure123",
+                         :password_confirmation=>"secure123")
   end
+
   describe "Registration" do
     it "should reject a registration if the date is invalid" do
-      @event.update_attributes(:start_date=>(Date.today - 1.week), :end_date => (Date.today - 1.day))
-      @new_registration.is_registerable? should_not == true
+      @event.update_attributes(:end_date => (Date.today - 1.day))
+      @new_registration.is_registerable?.should == {:date_error => "Registration date is not valid"}
+    end
+
+    it "should accept a registration if the date is valid" do
+      @new_registration.is_registerable?.should == true
+    end
+
+    describe "with a second registration" do
+      before(:each) do
+        @second_reg = GoldencobraEvents::EventRegistration.new
+        @second_reg.event_pricegroup = @event_pricegroup
+        @second_reg.user = @user2
+        @second_reg.save
+      end
+      it "should reject a registration if the maximum number of participants (event) is reached" do
+        @new_registration.is_registerable?.should == true
+        @event.update_attributes(:max_number_of_participators => 1)
+        @second_reg.is_registerable?.should == {:num_of_ev_part_reached => "Maximum number of participants reached for #{@event.title}"}
+      end
+  
+      it "should reject a registration if the pricegroup's maximum number of participants is reached" do
+        @new_registration.is_registerable?.should == true
+        @event_pricegroup.update_attributes(:max_number_of_participators => 1)
+        @second_reg.is_registerable?.should == {:num_of_pricegroup_part_reached => "Maximum number of participants reached for #{@event_pricegroup.pricegroup.title}"}
+      end
+    end
+
+    describe "with a parent event" do
+      before(:each) do
+        @parent_event = GoldencobraEvents::Event.create(:type_of_event => "No registration needed")
+        @event.update_attributes(:ancestry => "#{@parent_event.id}")
+      end
+      it "should approve the registration if parent doesn't need registering" do
+        @new_registration.is_registerable?.should == true
+      end
+
+      it "should reject the registration if parent needs registering and it's not registered yet" do
+        @parent_event.update_attributes(:type_of_event => "Registration needed")
+        @new_registration.is_registerable?.should == {:parent_error => [@parent_event.id] }
+      end
+
+      it "should reject the registration if there are multiple parents yet to register" do
+        @grandfather_event = GoldencobraEvents::Event.create(:type_of_event => "Registration needed")
+        @parent_event.update_attributes(:ancestry => "#{@grandfather_event.id}", :type_of_event => "Registration needed")
+        @event.update_attributes(:ancestry => "#{@grandfather_event.id}/#{@parent_event.id}")
+        @new_registration.is_registerable?.should == {:parent_error => [@parent_event.id, @grandfather_event.id]}
+      end
     end
   end
 end
