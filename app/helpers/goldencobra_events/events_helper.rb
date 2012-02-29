eb8df662579715aa5c21d758169cadfafd047f5d
Philipp Wilimzig <wilimzig@ikusei.de>
2012-02-29 10:09:02 +0100

    eigene css-klasse bei programmdarstellung

--- app/helpers/goldencobra_events/events_helper.rb
+++ app/helpers/goldencobra_events/events_helper.rb
@@ -14,9 +14,14 @@ module GoldencobraEvents
         class_name = options[:class] || ""
         content = ""
         content << event_item_helper(@article.event, depth, 1, options)
-        result = content_tag(:ul, raw(content), :class => "#{class_name} depth_#{depth} article_events level_1".squeeze(' ').strip)
-        result = content_tag(:div, raw(result), :id => "goldencobra_events_article_events", :class=> "#{@article.eventmoduletype} hidden")
         
+        if @article.eventmoduletype == "program"
+          result = content_tag(:ul, raw(content), :class => "#{class_name} depth_#{depth} article_events level_1".squeeze(' ').strip)
+          result = content_tag(:div, raw(result), :id => "goldencobra_events_article_events", :class=> "#{@article.eventmoduletype} eventprogramm")          
+        else
+          result = content_tag(:ul, raw(content), :class => "#{class_name} depth_#{depth} article_events level_1".squeeze(' ').strip)
+          result = content_tag(:div, raw(result), :id => "goldencobra_events_article_events", :class=> "#{@article.eventmoduletype} hidden")
+        end
         return_content = content_tag(:div, render(:partial => "goldencobra_events/events/webcode_form"), :id => "article_event_webcode_form" )
         return_content += result
         return_content += content_tag(:div, link_to(s("goldencobra_events.event.registration.enter_user_data"), "#", :id => "goldencobra_events_enter_account_data", :class => "button"), :id => "goldencobra_events_enter_account_data_wrapper", :style => "display:none")
