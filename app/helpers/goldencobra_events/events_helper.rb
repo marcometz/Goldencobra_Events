8de4fbec0415b68573935f9573b9f2dda3bd2626
Holger Frohloff <holger.frohloff@gmail.com>
2012-03-01 15:16:31 +0100

    typo

--- app/helpers/goldencobra_events/events_helper.rb
+++ app/helpers/goldencobra_events/events_helper.rb
@@ -130,7 +130,7 @@ module GoldencobraEvents
       
       #Anmeldelink anzeigen
       if (event.needs_registration? || event.registration_optional?) && @article.eventmoduletype == "registration" && !event.is_root?
-        reg_link = link_to("Hinzufügen", "/goldencobra_events/event/#{event.id}/register" ,:remote => true, :id => "register_for_event_#{event.id}_link", :class => "button")
+        reg_link = link_to("Hinzuf&uuml;gen", "/goldencobra_events/event/#{event.id}/register" ,:remote => true, :id => "register_for_event_#{event.id}_link", :class => "button")
         content << content_tag(:div, reg_link, :class => "register_for_event", "data-id" => event.id, :id => "register_for_event_#{event.id}")
       end
 
