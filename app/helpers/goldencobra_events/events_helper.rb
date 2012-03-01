c4f2642528dcc023526e9cc612b9c010574abab8
Philipp Wilimzig <wilimzig@ikusei.de>
2012-03-01 18:53:31 +0100

    bla

--- app/helpers/goldencobra_events/events_helper.rb
+++ app/helpers/goldencobra_events/events_helper.rb
@@ -74,7 +74,7 @@ module GoldencobraEvents
           end
         end  
 
-        c_start = content_tag(:div, raw(child.title ), class: 'child-title')) if !child.is_root? && @article.eventmoduletype == "registration" && child.has_children?
+        c_start = content_tag(:div, raw(child.title ), class: 'child-title') if !child.is_root? && @article.eventmoduletype == "registration" && child.has_children?
         cp = ""
         cp << content_tag(:div, raw(c_start), class: 'parent_title') if !child.is_root? && child.parent.start_date && @article.eventmoduletype == "registration" && child.has_children?
         cp << content_tag(:div, "#{raw(child.start_date.strftime('%H:%M'))} bis #{raw(child.end_date.strftime('%H:%M'))}", class: 'start_to_end_time_child') if !child.is_root? && child.start_date && child.end_date && @article.eventmoduletype == "registration" && child.has_children?
