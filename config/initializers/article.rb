7d51161b1883d28240045d60a7a183467ab83cd5
Holger Frohloff <holger.frohloff@gmail.com>
2012-03-02 09:06:42 +0100

    modified:   app/views/goldencobra_events/event_registration_mailer/registration_email.html.erb 	modified:   config/initializers/article.rb 	modified:   test/dummy/config/environments/production.rb

--- config/initializers/article.rb
+++ config/initializers/article.rb
@@ -3,17 +3,4 @@ Rails.application.config.to_prepare do
     belongs_to :event, class_name: GoldencobraEvents::Event, foreign_key: "event_id"
   end
 
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.perform_deliveries = true
-
-  ActionMailer::Base.smtp_settings = {
-    address: "mail.ikusei.de",
-    port: 25,
-    domain: "ikusei.de",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "holger+ikusei.de",
-    password: "mVFtiO3dRKtV",
-    enable_starttls_auto: false
-  }
 end
