4cea9680244a5fc3df1b3a2d138c3f7574ca817c
Holger Frohloff <holger.frohloff@gmail.com>
2012-03-14 16:08:43 +0100

    [#25996545] Sidebar für Rednerliste zum Artikel hinzugefügt Ausgabe im Frontend fehlt noch 	modified:   app/admin/articles.rb 	new file:   app/views/goldencobra_events/admin/events/_artist_list_module_sidebar.html.erb 	modified:   config/initializers/article.rb 	modified:   config/locales/en.yml 	new file:   db/migrate/20120314134333_add_artist_list_to_goldencobra_article.rb

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -5,5 +5,9 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
   sidebar :event_module, :only => [:edit] do      
     render "/goldencobra_events/admin/events/event_module_sidebar"
   end
+
+  sidebar :artist_list_module, only: [:edit] do
+    render "/goldencobra_events/admin/events/artist_list_module_sidebar"
+  end
   
 end
