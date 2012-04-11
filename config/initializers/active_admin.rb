6259cdb92fdddbd687c79627a1bac0528a87786f
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-11 15:24:43 +0200

    modified:   app/admin/articles.rb 	modified:   config/initializers/active_admin.rb

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.setup do |config|
-  # unless Rails.env == "production"
+  unless Rails.env == "production"
     config.load_paths = ["#{GoldencobraEvents::Engine.root}/app/admin/"]
-  # end
+  end
   # config.load_paths << "#{Goldencobra::Engine.root}/app/admin/"
   # config.load_paths = config.load_paths.uniq
   config.register_stylesheet 'goldencobra_events/active_admin'
