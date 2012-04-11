a651695810c77c6e417c4a1e338dd4639ba75b55
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-11 15:48:21 +0200

    modified:   Gemfile 	modified:   Gemfile.lock 	modified:   app/admin/articles.rb 	modified:   config/initializers/active_admin.rb

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -2,14 +2,14 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
 
   menu :parent => "Content-Management", :label => "Artikel"
   
-  # sidebar "veranstaltungs_modul", :only => [:edit] do      #:event_module
-  #   render "/goldencobra_events/admin/events/event_module_sidebar"
-  # end
-  # 
-  # sidebar "rednerlisten_modul", only: [:edit] do
-  #   render "/goldencobra_events/admin/events/artist_list_module_sidebar"
-  # end
-  # 
+  sidebar "veranstaltungs_modul", :only => [:edit] do      #:event_module
+    render "/goldencobra_events/admin/events/event_module_sidebar"
+  end
+
+  sidebar "rednerlisten_modul", only: [:edit] do
+    render "/goldencobra_events/admin/events/artist_list_module_sidebar"
+  end
+
   sidebar "sponsoren_modul", only: [:edit] do
     render "/goldencobra_events/admin/events/sponsor_module_sidebar"
   end
