926bc8bcf137bac8ce75c65fe67162e5ce9d5737
Marco Metz <marco.metz@gmail.com>
2012-04-11 17:21:23 +0200

    modified:   app/admin/pricegroups.rb

--- app/admin/pricegroups.rb
+++ app/admin/pricegroups.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register GoldencobraEvents::Pricegroup, :as => "Pricegroup" do
     
-    menu :parent => "Event-Management", :label => "Preisgruppen"
+  menu :parent => "Event-Management", :label => "Preisgruppen"
+  
+  filter :title
 
   action_item :only => :show do
     link_to(t('active_admin.pricegroups.new_pricegroup'), new_admin_pricegroup_path)
