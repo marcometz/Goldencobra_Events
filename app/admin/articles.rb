2138692a747b6f5a5a7be3466646f6979b63bc0b
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-11 15:25:13 +0200

    modified:   app/admin/articles.rb

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -10,8 +10,8 @@ ActiveAdmin.register Goldencobra::Article, :as => "Article" do
   #   render "/goldencobra_events/admin/events/artist_list_module_sidebar"
   # end
   # 
-  # sidebar "sponsoren_modul", only: [:edit] do
-  #   render "/goldencobra_events/admin/events/sponsor_module_sidebar"
-  # end
+  sidebar "sponsoren_modul", only: [:edit] do
+    render "/goldencobra_events/admin/events/sponsor_module_sidebar"
+  end
   
 end
