2cc09663f45135fa3fcb742f4cbddb42f0f35333
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-12 16:57:01 +0200

    modified:   app/models/goldencobra_events/registration_user.rb

--- app/models/goldencobra_events/registration_user.rb
+++ app/models/goldencobra_events/registration_user.rb
@@ -32,7 +32,7 @@ module GoldencobraEvents
     def total_price
       total_price = 0
       self.event_registrations.each do |e|
-        total_price += e.event_pricegroup.price
+        total_price += e.event_pricegroup.price if e.event_pricegroup && e.event_pricegroup.price
       end
       return total_price
     end
