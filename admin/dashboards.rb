cbe4953feb1a7793270be3e9525208cc8098e4d8
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-12 12:29:33 +0200

    modified:   admin/dashboards.rb

--- admin/dashboards.rb
+++ admin/dashboards.rb
@@ -1,22 +1,19 @@
 ActiveAdmin::Dashboards.build do
-  
-  if can?(:read, GoldencobraEvents::RegistrationUser)
-    section "Neueste Anmeldungen" do
-      table do
+  section "Neueste Anmeldungen" do
+    table do
+      tr do
+        [t('attributes.user.complete_name'), t('attributes.user.email'), t('attributes.user.created_at'), t('attributes.user.details')].each do |sa|
+          th sa
+        end
+      end
+     
+      GoldencobraEvents::RegistrationUser.order("created_at DESC").limit(10).collect do |applicant|
         tr do
-          [t('attributes.user.complete_name'), t('attributes.user.email'), t('attributes.user.created_at'), t('attributes.user.details')].each do |sa|
-            th sa
-          end
-        end
-       
-        GoldencobraEvents::RegistrationUser.order("created_at DESC").limit(10).collect do |applicant|
-          tr do
-            td "#{applicant.lastname}, #{applicant.firstname}"
-            td applicant.email
-            td l(applicant.created_at, format: :long)
-            td link_to("Details", admin_applicant_path(applicant))
-          end        
-        end
+          td "#{applicant.lastname}, #{applicant.firstname}"
+          td applicant.email
+          td l(applicant.created_at, format: :long)
+          td link_to("Details", admin_applicant_path(applicant))
+        end        
       end
     end
   end
