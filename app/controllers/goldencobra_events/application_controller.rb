7f06e5e426253604c58d22eaeeba4dc5765cf453
Holger Frohloff <holger.frohloff@gmail.com>
2012-04-23 09:53:39 +0200

    cancan rescue 	modified:   app/controllers/goldencobra_events/application_controller.rb

--- app/controllers/goldencobra_events/application_controller.rb
+++ app/controllers/goldencobra_events/application_controller.rb
@@ -1,4 +1,7 @@
 module GoldencobraEvents
   class ApplicationController < ActionController::Base
+    rescue_from CanCan::AccessDenied do |exception|
+      redirect_to root_url, :alert => exception.message
+    end
   end
 end
