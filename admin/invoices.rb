d852203b9fab40957144e4fb1d324d9b696e4515
Holger Frohloff <holger.frohloff@gmail.com>
2012-09-10 17:53:53 +0200

    ticketaenderungen wie angegeben 	modified:   admin/invoices.rb 	modified:   app/views/templates/ticket/ticket.html.erb

--- admin/invoices.rb
+++ admin/invoices.rb
@@ -42,6 +42,9 @@ ActiveAdmin.register GoldencobraEvents::RegistrationUser, :as => "Invoice" do
     column "Rechnung" do |i|
       link_to(i.event_registrations.first.invoice_number, "/system/invoices/#{i.event_registrations.first.invoice_number}.pdf", target: "blank") if i.event_registrations.count > 0 && i.event_registrations.first.invoice_number.present?
     end
+    column "Ticket" do |applicant|
+      link_to(applicant.event_registrations.first.ticket_number, "/system/tickets/ticket_#{applicant.event_registrations.first.ticket_number}.pdf", target: "blank") if applicant.event_registrations.count > 0 && applicant.event_registrations.first.ticket_number.present?
+    end
     column :invoice_sent, sortable: :invoice_sent do |invoice|
       invoice.invoice_sent.strftime("%d.%m.%Y") if invoice.invoice_sent
     end
