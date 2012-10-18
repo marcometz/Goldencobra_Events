83f0339c7358b9ce99728cca369c615749a5fff6
Holger Frohloff <holger.frohloff@gmail.com>
2012-10-18 13:38:46 +0200

    Mailer für BounceReport erstellt

---
+++ app/mailers/goldencobra_events/bouncer_mailer.rb
@@ -0,0 +1,48 @@
+module GoldencobraEvents
+  class BouncerMailer < ActionMailer::Base
+    default from: Goldencobra::Setting.for_key("goldencobra_events.imap.bouncer_mailer.from")
+    default subject: Goldencobra::Setting.for_key("goldencobra_events.imap.bouncer_mailer.subject")
+    default :content_type => "text/html"
+    default :reply_to => Goldencobra::Setting.for_key("goldencobra_events.imap.bouncer_mailer.reply_to")
+
+    def report_bouncing(email = nil, counter = 0)
+      @counter = counter
+      @url = Goldencobra::Setting.for_key('goldencobra.url')
+      if email.present?
+        mail to:email bcc: 'holger@ikusei.de'
+      else
+        logger.error 'BouncerMailer needs an email to send report.'
+        do_not_deliver!
+      end
+    end
+  end
+end
+
+# http://stackoverflow.com/questions/6550809/rails-3-how-to-abort-delivery-method-in-actionmailer
+
+module ActionMailer
+  class Base
+    # A simple way to short circuit the delivery of an email from within
+    # deliver_* methods defined in ActionMailer::Base subclases.
+    def do_not_deliver!
+      raise AbortDeliveryError
+    end
+
+    def process(*args)
+      begin
+        super *args
+      rescue AbortDeliveryError
+        self.message = BlackholeMailMessage
+      end
+    end
+  end
+end
+
+class AbortDeliveryError < StandardError
+end
+
+class BlackholeMailMessage < Mail::Message
+  def self.deliver
+    false
+  end
+end
--- app/models/goldencobra_events/bouncer.rb
+++ app/models/goldencobra_events/bouncer.rb
@@ -11,11 +11,13 @@ module GoldencobraEvents
         imap.authenticate('LOGIN',Goldencobra::Setting.for_key('goldencobra_events.imap.login'),Goldencobra::Setting.for_key('goldencobra_events.imap.password'))
         imap.select('INBOX')
         bounce_results = []
+        counter = 0
         imap.search(["NOT", "DELETED"]).each do |message_id|
           # Use gem 'mail' to check for mail status
           mail = Mail::Message.new(imap.fetch(message_id, "RFC822")[0].attr["RFC822"])
           if mail.bounced?
-            puts "#{mail.final_recipient.split("rfc822;")[1]} bounced, status: #{mail.error_status}, retryable: #{mail.retryable?}"
+            counter += 1
+            # puts "#{mail.final_recipient.split("rfc822;")[1]} bounced, status: #{mail.error_status}, retryable: #{mail.retryable?}"
             address = mail.final_recipient.split("rfc822;")[1]
             blacklist_entry = GoldencobraEvents::EmailBlacklist.find_by_email_address(address) || GoldencobraEvents::EmailBlacklist.new(email_address: address)
             if blacklist_entry.count
@@ -29,6 +31,11 @@ module GoldencobraEvents
           end
           # delete message after parsing
           # imap.store(message_id, "+FLAGS", [:Deleted])
+        end
+
+        # Send bounce report
+        if Goldencobra::Setting.for_key('goldencobra_events.imap.send_bounce_report') == 'true'
+          GoldencobraEvents::BouncerMailer.report_bouncing(Goldencobra::Setting.for_key('goldencobra_events.imap.bounce_recipient'), counter).deliver
         end
       end
     end
