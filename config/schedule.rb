c5e899443c2d57141b4a480ff6c7713806823478
Holger Frohloff <holger.frohloff@gmail.com>
2012-10-18 13:58:41 +0200

    Syntax!

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,4 @@
 # Use gem 'whenever' to schedule a cron job to peridodically check for bounced emails in INBOX
 every :day, :at => '01:00am' do
-  if Goldencobra::Setting.for_key('goldencobra_events.imap.use_blacklist') == "true"
-    runner "GoldencobraEvents::Bouncer.check_bounce_status"
-  end
+  runner "GoldencobraEvents::Bouncer.check_bounce_status"
 end
