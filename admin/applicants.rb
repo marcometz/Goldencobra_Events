d33a0c375beb7b7b7885ffb1d83d4a26420a85f9
Marco Metz <marco.metz@gmail.com>
2013-03-12 16:14:19 +0100

    modified:   admin/applicants.rb

--- admin/applicants.rb
+++ admin/applicants.rb
@@ -31,7 +31,7 @@ ActiveAdmin.register GoldencobraEvents::RegistrationUser, :as => "Applicant" do
       span applicant.email, class: 'email'
     end
     column "Event" do |applicant|
-      applicant.event_pricegroup.event.title if applicant.event_pricegroup && applicant.event_pricegroup.event
+      applicant.event_registrations.last.event.title if applicant.event_registrations.last && applicant.event_registrations.last.event
     end
     column 'Ticket' do |applicant|
       link_to(applicant.event_registrations.first.ticket_number, "/system/tickets/ticket_#{applicant.event_registrations.first.ticket_number}.pdf", target: 'blank') if applicant.event_registrations.count > 0 && applicant.event_registrations.first.ticket_number.present?
